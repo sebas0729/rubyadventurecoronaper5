ea4ce9c8a49721e61079ca6775e54d7ef9c4a519
sebas0729 <730255@student.dusd.net>
2023-11-15 13:48:59 -0800

    ruby movement
    added ruby movement horizontal and vertical

--- rubyadventureper5corona/Assets/Scripts/RubyControlleer.cs
+++ rubyadventureper5corona/Assets/Scripts/RubyControlleer.cs
@@ -13,10 +13,14 @@ public class rubycontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("horizontal");
-        Debug.Log(horizontal);
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
-        Vector2  position = transform.position;
+
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f + horizontal;
+        position.y = position.y + 0.1f + vertical;
+
         transform.position = position;
     }
 }
